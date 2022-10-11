e034216b93cc001b3da4746a436799fb650e2aff
Anurag Banerjee <anurag@axioned.com>
2022-10-11 12:35:09 +0530

    feat: add gtm

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,6 +49,10 @@ const config = {
 				theme: {
 					customCss: require.resolve("./src/css/custom.css"),
 				},
+				gtag: {
+					trackingID: "G-XET7E8E4LW",
+					anonymizeIP: true,
+				},
 			}),
 		],
 	],
