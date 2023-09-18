e7a1ccb5758b519a689e591744ffb16cfeb23230
inayatullahaaxioned <inayatullaha@axioned.com>
2023-09-18 20:27:25 +0530

    github deploy intial setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://axionedhandbook.github.io',
+  base: '/handbook/tree/astro',
   integrations: [
     starlight({
       title: "Axioned Handbook",
