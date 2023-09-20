a5304bd3d9e5c6d4c39a62d4902e8374d57d1d49
Anurag Banerjee <anurag@axioned.com>
2023-09-20 13:58:38 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import starlight from "@astrojs/starlight";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://axionedhandbook.github.io',
-  base: '/handbook/tree/astro',
+  site: "https://axionedhandbook.github.io",
+  base: "/",
   integrations: [
     starlight({
       title: "Axioned Handbook",
