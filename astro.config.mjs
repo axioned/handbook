96436ea2885c344ea79e98d542b3afbb79a532d2
Anurag Banerjee <anurag@axioned.com>
2023-09-21 14:57:34 +0530

    Add logo and update theme config

--- astro.config.mjs
+++ astro.config.mjs
@@ -11,26 +11,41 @@ export default defineConfig({
       social: {
         github: "https://github.com/axioned/handbook",
       },
+      editLink: {
+        baseUrl: "https://github.com/axioned/handbook/edit/main/",
+      },
+      tableOfContents: true,
+      logo: {
+        src: "/public/logo.png",
+        alt: "Axioned",
+      },
+      lastUpdated: true,
       sidebar: [
+        {
+          label: "Welcome",
+          link: "/welcome",
+        },
         {
           label: "Playbook",
           autogenerate: { directory: "playbook" },
         },
         {
           label: "Learning",
+          collapsed: true,
           autogenerate: { directory: "learning" },
         },
         {
           label: "Processes",
+          collapsed: true,
           autogenerate: { directory: "processes" },
         },
         {
-          label: "Guides",
-          items: [
-            // Each item here is one entry in the navigation menu.
-            { label: "Glossary", link: "/glossary/" },
-            { label: "Contribute", link: "/contribute/" },
-          ],
+          label: "Glossary",
+          link: "/glossary/",
+        },
+        {
+          label: "Contribute",
+          link: "/contribute/",
         },
       ],
     }),
