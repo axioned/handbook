cb2188a30241d89319d39bfdbf5659e2b8a1faaa
Anurag Banerjee <anurag@axioned.com>
2025-05-21 17:04:16 +0530

    style: add prettier config

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,4 +1,12 @@
 module.exports = {
   ...require("@axioned/prettier-config"),
   plugins: ["prettier-plugin-astro"],
+  "overrides": [
+    {
+      "files": "*.astro",
+      "options": {
+        "parser": "astro",
+      },
+    },
+  ],
 };
